4fdc721dc89c987c380157a55c5181947f51728b
JoelDuff999 <42261062+JoelDuff999@users.noreply.github.com>
2021-03-17 13:36:24 +1100

    Basic XML handling, verbose flag, path flag

---
+++ main/Server.java
@@ -0,0 +1,4 @@
+public class Server {
+    //Accept list of args or whatever XML parser gives us.
+    public Server() {
+
--- main/ds_client_test.java
+++ main/ds_client_test.java
@@ -2,6 +2,7 @@ package main;
 
 import main.Connection;
 
+//Sockets
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
@@ -10,91 +11,129 @@ import java.util.HashMap;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
+//XML Parsing
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import java.io.File;
+
 public class ds_client_test {
     public static void main(String[] args) throws UnknownHostException, IOException {
-    	//TODO MANDATORY. Accept -a argument (scheduling algorithm name).
-    	//TODO MANDATORY Accept -p argument (port number).
-		
-		HashMap<String,String> parsedArgs = new HashMap<String,String>();
-		//Default values
-		parsedArgs.put("port", "50000");//
-		parsedArgs.put("-a", "atl");//AllToLargest
-    	
-		for (int i = 0; i < args.length; i++) {
-			switch(args[i]) {
-				case "-p":
-					i++;
-					parsedArgs.put("port", args[i]);
-					break;
-				case "-a":
-					i++;
-					parsedArgs.put("-a", args[i]);
-					break;
-				default:
-					System.err.println("Default called for " + args[i]);
-					break;
-			}
-		}
-		
-    	/*Socket socket = new Socket("localhost", Integer.parseInt(parsedArgs.get("-p")));
-    	PrintStream out = new PrintStream(socket.getOutputStream());
-    	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));*/
-		
-		Connection socket = new Connection("localhost", Integer.parseInt(parsedArgs.get("port")));
-		
-    	String message;
-    	
-    	//Handshake
-		//Print HELO to the console
-    	System.out.println("C: HELO");
-		//Send HELO to the server.
-    	socket.write("HELO");
-		//Server should send us one word back (OK), read a word and see if we received OK.
-    	message = socket.readWord();
-    	if (!message.equals("OK")) {
-    		socket.close();
-			//Throw an error if we don't get OK.
-    		throw new IOException("Handshake failed.\n(HELO did not recieve OK, received " + message + " instead.)");
-    	}
-		
-		//Print on the console that we received OK and are sending AUTH.
-    	System.out.println("S: OK\nC: AUTH Group_21");
-		//Send AUTH
-    	socket.write("AUTH Group_21");
-		//Read response (Should be OK)
-    	message = socket.readWord();
-    	if (!message.equals("OK")) {
-    		socket.close();
-			//Throw an error if we don't get OK
-    		throw new IOException("Handshake failed.\n(AUTH refused. OK expected, " + message + "received.)");
-    	}
-    	System.out.println("S: OK");
-    	
-    	//TODO Read ds-system.xml
-    	
-		//Finish the handshake.
-    	//System.out.println("C: REDY");
-    	//socket.write("REDY");
-    	
-    	socket.close();
+        //TODO MANDATORY. Accept -a argument (scheduling algorithm name).
+        //TODO MANDATORY Accept -p argument (port number).
+       
+        boolean verbose = false;
+        HashMap<String,String> parsedArgs = new HashMap<String,String>();
+        //Default values
+        parsedArgs.put("port", "50000");//
+        parsedArgs.put("-a", "atl");//AllToLargest
+        parsedArgs.put("syspath", "./ds_system.xml");
+        
+        for (int i = 0; i < args.length; i++) {
+            switch(args[i]) {
+                case "-p":
+                    i++;
+                    parsedArgs.put("port", args[i]);
+                    break;
+
+                case "-a":
+                    i++;
+                    parsedArgs.put("-a", args[i]);
+                    break;
+
+                case "-v":
+                    verbose = true;
+                    break;
+
+                case "-path":
+                    i++;
+                    parsedArgs.put("syspath", args[i]);
+                    break;
+
+                default:
+                    //Debug string. Should not occur.
+                    System.err.println("Default called for " + args[i]);
+                    break;
+            }
+        }
+        
+        /*Socket socket = new Socket("localhost", Integer.parseInt(parsedArgs.get("-p")));
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));*/
+        
+        Connection socket = new Connection("localhost", Integer.parseInt(parsedArgs.get("port")));
+        
+        String message;
+        
+        //Handshake
+        //Print HELO to the console
+        System.out.println("C: HELO");
+        //Send HELO to the server.
+        socket.write("HELO");
+        //Server should send us one word back (OK), read a word and see if we received OK.
+        message = socket.readWord();
+        if (!message.equals("OK")) {
+            socket.close();
+            //Throw an error if we don't get OK.
+            throw new IOException("Handshake failed.\n(HELO did not recieve OK, received " + message + " instead.)");
+        }
+        
+        //Print on the console that we received OK and are sending AUTH.
+        System.out.println("S: OK\nC: AUTH Group_21");
+        //Send AUTH
+        socket.write("AUTH Group_21");
+        //Read response (Should be OK)
+        message = socket.readWord();
+        if (!message.equals("OK")) {
+            socket.close();
+            //Throw an error if we don't get OK
+            throw new IOException("Handshake failed.\n(AUTH refused. OK expected, " + message + "received.)");
+        }
+        System.out.println("S: OK");
+        
+        //TODO Read ds-system.xml
+        
+        try {
+            File xml = new File(parsedArgs.get("syspath"));//TODO VERIFY THIS ADDRESS
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xml);
+            doc.getDocumentElement().normalize();
+            
+            NodeList servers = doc.getElementsByTagName("servers");
+            for (int i = 0; i < servers.getLength(); i++) {
+                //PARSE SERVERS
+                continue;
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+        //Finish the handshake.
+        System.out.println("C: REDY");
+        socket.write("REDY");
+        System.out.println(socket.readWord());
+        socket.close();
+
         return;
     }
     
     /**
-	 * Defunct
+     * Defunct
      * Assumes the first character is a letter in order to enforce blocking.
      *
     public static String readWord(BufferedReader in) throws IOException {
-    	String ret = Character.toString((char) in.read());
-    	int ch;
-    	while (in.ready()) {
-    		ch = in.read();
-    		if (ch == 32) {//Space
-    			return ret;
-    		}
-    		ret = ret + Character.toString((char) ch);
-    	}
-    	return ret;
+        String ret = Character.toString((char) in.read());
+        int ch;
+        while (in.ready()) {
+            ch = in.read();
+            if (ch == 32) {//Space
+                return ret;
+            }
+            ret = ret + Character.toString((char) ch);
+        }
+        return ret;
     }*/
     
     /**
@@ -106,6 +145,6 @@ public class ds_client_test {
      * @return If the next message from the server is "OK".
      *//*
     public static boolean ok(BufferedReader in) throws IOException {
-    	return readWord(in).equals("OK");
+        return readWord(in).equals("OK");
     }*/
 }
